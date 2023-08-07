10010e43888c25de5deb599e185b26b1684bfdd0
medilies <medilies@gmail.com>
2023-08-07 21:08:32 +0100

    cleanup framework skeleton

--- config/filesystems.php
+++ config/filesystems.php
@@ -6,11 +6,6 @@ return [
     |--------------------------------------------------------------------------
     | Default Filesystem Disk
     |--------------------------------------------------------------------------
-    |
-    | Here you may specify the default filesystem disk that should be used
-    | by the framework. The "local" disk, as well as a variety of cloud
-    | based disks are available to your application. Just store away!
-    |
     */
 
     'default' => env('FILESYSTEM_DISK', 'local'),
@@ -19,13 +14,6 @@ return [
     |--------------------------------------------------------------------------
     | Filesystem Disks
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure as many filesystem "disks" as you wish, and you
-    | may even configure multiple disks of the same driver. Defaults have
-    | been set up for each driver as an example of the required values.
-    |
-    | Supported Drivers: "local", "ftp", "sftp", "s3"
-    |
     */
 
     'disks' => [
@@ -62,11 +50,6 @@ return [
     |--------------------------------------------------------------------------
     | Symbolic Links
     |--------------------------------------------------------------------------
-    |
-    | Here you may configure the symbolic links that will be created when the
-    | `storage:link` Artisan command is executed. The array keys should be
-    | the locations of the links and the values should be their targets.
-    |
     */
 
     'links' => [
--- config/hashing.php
+++ config/hashing.php
@@ -6,13 +6,6 @@ return [
     |--------------------------------------------------------------------------
     | Default Hash Driver
     |--------------------------------------------------------------------------
-    |
-    | This option controls the default hash driver that will be used to hash
-    | passwords for your application. By default, the bcrypt algorithm is
-    | used; however, you remain free to modify this option if you wish.
-    |
-    | Supported: "bcrypt", "argon", "argon2id"
-    |
     */
 
     'driver' => 'bcrypt',
@@ -21,11 +14,6 @@ return [
     |--------------------------------------------------------------------------
     | Bcrypt Options
     |--------------------------------------------------------------------------
-    |
-    | Here you may specify the configuration options that should be used when
-    | passwords are hashed using the Bcrypt algorithm. This will allow you
-    | to control the amount of time it takes to hash the given password.
-    |
     */
 
     'bcrypt' => [
@@ -36,11 +24,6 @@ return [
     |--------------------------------------------------------------------------
     | Argon Options
     |--------------------------------------------------------------------------
-    |
-    | Here you may specify the configuration options that should be used when
-    | passwords are hashed using the Argon algorithm. These will allow you
-    | to control the amount of time it takes to hash the given password.
-    |
     */
 
     'argon' => [
