44f150709fccf4f2f301f5c7a89b766dc586d58a
medilies <medilies@gmail.com>
2023-08-07 21:27:19 +0100

    improve /render rule

--- app/Http/Controllers/RenderController.php
+++ app/Http/Controllers/RenderController.php
@@ -4,25 +4,35 @@ namespace App\Http\Controllers;
 
 use ElaborateCode\RowBloom\RowBloom;
 use ElaborateCode\RowBloom\Support;
-use Illuminate\Support\Facades\Response;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response as HttpResponse;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Validation\Rule;
 
 class RenderController
 {
-    public function __invoke(Request $request, RowBloom $rowBloom): HttpResponse
+    public function __invoke(Request $request, RowBloom $rowBloom, Support $support): HttpResponse
     {
         $request->validate([
-            'interpolatorDriver' => ['required', 'string'],
-            'rendererDriver' => ['required', 'string'],
+            'interpolatorDriver' => [
+                'required',
+                'string',
+                Rule::in(array_keys($support->getInterpolatorDrivers())),
+            ],
+            'rendererDriver' => [
+                'required',
+                'string',
+                Rule::in(array_keys($support->getRendererDrivers())),
+            ],
             'template' => ['required', 'string'],
             'css' => ['required', 'string'],
-            'table' => ['required', 'array'],
+            'table' => ['required', 'array', 'min:1'],
+            'table.*' => ['required', 'array'],
             'options' => ['required', 'array'],
         ]);
 
-        $rowBloom->setInterpolator(app()->get(Support::class)->getInterpolatorDrivers()[$request->interpolatorDriver])
-            ->setRenderer(app()->get(Support::class)->getRendererDrivers()[$request->rendererDriver]);
+        $rowBloom->setInterpolator($support->getInterpolatorDrivers()[$request->interpolatorDriver])
+            ->setRenderer($support->getRendererDrivers()[$request->rendererDriver]);
 
         $rowBloom->addTable($request->table);
         $rowBloom->setTemplate($request->template);
