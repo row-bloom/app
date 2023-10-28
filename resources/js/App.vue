e57d5dd9e631b767541fb4cab313997f5fae0a68
medilies <medilies@gmail.com>
2023-10-28 20:57:59 +0100

    dynamic table

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -17,36 +17,45 @@
         </div>
         <div class="h-[10%] min-h-[200px] flex flex-col">
             <div class="w-full overflow-auto">
-                <table class="w-full text-sm">
-                    <tr class="border-b">
-                        <th class="h-12 px-4 font-medium align-middle">
-                            Column 1
-                        </th>
-                        <th class="h-12 px-4 font-medium align-middle">
-                            Column 2
-                        </th>
-                        <th class="h-12 px-4 font-medium align-middle">
-                            Column 3
-                        </th>
-                    </tr>
-                    <tr class="border-b">
-                        <td class="p-4 align-middle">Data 1</td>
-                        <td class="p-4 align-middle">Data 2</td>
-                        <td class="p-4 align-middle">Data 3</td>
-                    </tr>
-                </table>
+                <DataTable
+                    :value="renderStore.table.data"
+                    tableStyle="min-width: 50rem"
+                >
+                    <Column
+                        v-for="col of columns"
+                        :key="col"
+                        :field="col"
+                        :header="col"
+                    ></Column>
+                </DataTable>
             </div>
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+
+import { computed } from "vue";
+
 import useSupportStore from "@/stores/support";
 import HtmlEditor from "@/components/editors/HtmlEditor.vue";
 import CssEditor from "@/components/editors/CssEditor.vue";
 import RenderAction from "@/components/render/RenderAction.vue";
 import RenderingOptions from "@/components/options/RenderingOptions.vue";
 import ParseTable from "@/components/tables/ParseTable.vue";
+
+import useRenderStore from "@/stores/render";
+
+const renderStore = useRenderStore();
+
+const columns = computed(() => {
+    if (renderStore.table.data.length > 0) {
+        return Object.keys(renderStore.table.data[0]);
+    }
+    return [];
+});
 
 useSupportStore().init();
 </script>
